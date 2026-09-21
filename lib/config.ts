7f8179f244dda69486d554e22d6e958aad33e754
bmenezz07 <breno.c.mascarenhas@gmail.com>
2026-09-21 01:52:32 -0300

    Add lib/config.ts

---
+++ lib/config.ts
@@ -0,0 +1,3 @@
+export const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL || "https://lomforzrwmhtuvtibivx.supabase.co";
+export const SUPABASE_KEY = process.env.NEXT_PUBLIC_SUPABASE_PUBLISHABLE_KEY || "sb_publishable_yry_lqqDcvPPi9N1Z4XFDg_7u6hoeup";
+export const EVENT_CODE = "earthdance-rs-2026";
